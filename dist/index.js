2202546abb04a68396169fc150ea9e1d3302fd89
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-03 17:56:06 +0100

    03.03

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+"use strict";
+// ===== 01 - If Statements ===== //
+// if (true) {
+//     console.log('This is true');
+// }
+// if (false) {
+//     console.log('This is not true');
+// }
+// const x: number = 20;
+// const y: number = 50;
+// if (x >= y) {
+//     console.log(`${x} is grater than or equal to ${y}`);
+// }
+// if (x === y) {
+//     console.log(`${x} is equal to ${y}.`);
+// } else {
+//     console.log(`${x} is not equal to ${y}.`);
+// }
+// if (x !== y) {
+//     const z: number = 20;
+//     console.log(`${z} is 20`);
+// }
+// Shorthand If
+// if (x >= y) console.log(`${x} is grater than or equal to ${y}`);
+// else console.log('This is false');
+// const x: number = 4;
+// if (x > 5) {
+//     console.log(`${x} is greater than 5.`);
+// } else {
+//     console.log(`${x} is less than or equal to 5.`);
+// }
