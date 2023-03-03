a533473dc63cfef082fdbb75715e942c63ab2832
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-03 18:22:17 +0100

    03.03

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 "use strict";
-// ===== 01 - If Statements ===== //
+// ===== 01 If Statements ===== //
 // if (true) {
 //     console.log('This is true');
 // }
@@ -29,3 +29,56 @@
 // } else {
 //     console.log(`${x} is less than or equal to 5.`);
 // }
+// ===== 02 Else-if and Nesting ===== //
+// const d: Date = new Date(10, 30, 2022, 20, 0, 0);
+// const hour: number = d.getHours();
+// if (hour < 12) {
+//     console.log('Good Morning');
+// } else if (hour < 18) {
+//     console.log('Good Afternonn');
+// } else {
+//     console.log('Good Night');
+// }
+// Nested If 
+// if (hour < 12) {
+//     console.log('Good Morning');
+//     if (hour === 6) {
+//         console.log('Wake Up');
+//     }
+// } else if (hour < 18) {
+//     console.log('Good Afternonn');
+// } else {
+//     console.log('Good Night');
+//     if(hour >= 20) {
+//         console.log('zzzzzzzz');
+//     }
+// }
+// if (hour >= 7 && hour < 15) {
+//     console.log('It is work time');
+// }
+// if (hour === 6 || hour === 20) {
+//     console.log('Brush your teeth!');
+// }
+const date = new Date();
+const hour = date.getHours();
+if (hour < 12) {
+    console.log('Good Morning!');
+    if (hour === 6) {
+        console.log('Wake up!');
+    }
+}
+else if (hour < 18) {
+    console.log('Good Afternoon!');
+}
+else {
+    console.log('Good Night!');
+    if (hour >= 20) {
+        console.log('zzzzzzz');
+    }
+}
+if (hour >= 7 && hour < 15) {
+    console.log('It is work time!');
+}
+if (hour === 6 || hour === 20) {
+    console.log('Brush your teeth!');
+}
